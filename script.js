77b4435b319982bf658a1cf02828b11a285eb626
Florin277 <simoncaflorin79@gmail.com>
2025-04-02 19:20:25 +0300

    Commit 1

---
+++ script.js
@@ -0,0 +1,17 @@
+function buttonOneFunction() {
+    document.getElementById("first").innerText="Castigator";
+    document.getElementById("second").innerText="";
+    document.getElementById("third").innerText="";
+    
+    
+}
+function buttonTwoFunction() {
+    document.getElementById("second").innerText="Necastigator";
+    document.getElementById("first").innerText=" ";
+    document.getElementById("third").innerText="";
+}
+function buttonThreeFunction() {
+    document.getElementById("third").innerText ="Necastigator";
+    document.getElementById("first").innerText="";
+    document.getElementById("second").innerText="";
+}
